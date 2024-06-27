53a4e564535e2d7025353dffc9dfddb073674bfa
moglinen77 <moglinen77@gmail.com>
2024-06-27 20:31:29 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,32 @@
+my_dist = { 'debit: ': 23456.34 , 'credit: ' : 1345.56 , 'sal: ' : 'a'}
+print(my_dist['credit: '])
+print(my_dist)
+my_dist.update({'zaym: ' : 4576 , 'otcat: ' : 5869})
+print(my_dist)
+a = my_dist.pop('otcat: ')
+print(a)
+print(my_dist.get('otcat'))
+print(my_dist)
+
+my_set = { 2345 , 'Cat: ', ( True , 1), 1, 3, 4, 5, 5}
+print(my_set)
+print(my_set.remove(5))
+print(my_set)
+print(my_set.add('kalosha'))
+print(my_set)
+my_set.update({45,54})
+print(my_set)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
